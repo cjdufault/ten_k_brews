fae9daadbc32125dc42f908ca3f9c21fa5475660
cjdufault <dufaultconnor@gmail.com>
2021-03-04 10:51:51 -0600

    update establishment type choices

--- ten_k_brews_app/models.py
+++ ten_k_brews_app/models.py
@@ -4,18 +4,25 @@ from django.db import models
 # Create your models here.
 class Establishment(models.Model):
 
-    # enum options for type of establishment, ex: Establishment.EstablishmentType.BREWERY == 1
-    class EstablishmentType(models.IntegerChoices):
-        BREWERY = 1
-        WINERY = 2
-        DISTILLERY = 3
-        CIDERY = 4
-        OTHER = 5
+    # choices for establishment types
+    BREWERY = 'Brewery'
+    WINERY = 'Winery'
+    DISTILLERY = 'Distillery'
+    CIDERY = 'Cidery'
+    OTHER = 'Other'
+
+    ESTABLISHMENT_TYPES = (
+        (BREWERY, 'Brewery'),   # (actual value, human readable value)
+        (WINERY, 'Winery'),
+        (DISTILLERY, 'Distillery'),
+        (CIDERY, 'Cidery'),
+        (OTHER, 'Other')
+    )
 
     # descriptive variables
     name = models.CharField(max_length=50, blank=False)
     description = models.CharField(max_length=500, blank=True)
-    establishment_type = models.IntegerField(choices=EstablishmentType.choices, blank=False)
+    establishment_type = models.CharField(max_length=10, choices=ESTABLISHMENT_TYPES, blank=False)
 
     # location variables
     address = models.CharField(max_length=100, blank=False)
@@ -24,30 +31,27 @@ class Establishment(models.Model):
     zip_code = models.CharField(max_length=5, blank=False)
 
     def __str__(self):
-        if self.establishment_type == 1:
-            type_string = 'Brewery'
-        elif self.establishment_type == 2:
-            type_string = 'Winery'
-        elif self.establishment_type == 3:
-            type_string = 'Distillery'
-        elif self.establishment_type == 4:
-            type_string = 'Cidery'
-        else:
-            type_string = 'Other'
-
-        return f'{self.name} ({self.city}) -- {type_string}'
+        return f'{self.name} -- {self.establishment_type} -- {self.city}'
 
 
 class Drink(models.Model):
 
-    # enum options for type of drink, ex: Drink.DrinkType.BEER == 1
-    class DrinkType(models.IntegerChoices):
-        BEER = 1
-        WINE = 2
-        LIQUOR = 3
-        CIDER = 4
+    BEER = 'Beer'
+    WINE = 'Wine'
+    LIQUOR = 'Liquor'
+    CIDER = 'Cider'
+    OTHER = 'Other'
+
+    DRINK_TYPES = (
+        (BEER, 'Beer'),
+        (WINE, 'Wine'),
+        (LIQUOR, 'Liquor'),
+        (CIDER, 'Cider'),
+        (OTHER, 'Other')
+    )
 
     name = models.CharField(max_length=100, blank=False)
+    drink_type = models.CharField(max_length=10, choices=DRINK_TYPES, blank=False)
     style = models.CharField(max_length=100, blank=True)
     description = models.CharField(max_length=500, blank=True)
 
